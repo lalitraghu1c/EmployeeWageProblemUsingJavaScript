dbb24c6770224926cebaa195b913c36dd16a8c82
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-12-28 08:39:15 +0530

    [Lalit] Add: Check Wage for part time or full time

---
+++ Employee.js
@@ -0,0 +1,31 @@
+//UC-1
+const IS_PRESENT = 0;
+let empCheck = Math.floor(Math.random() * 10 % 2 );
+if (empCheck == IS_PRESENT)
+{
+    console.log("Emp is Absent");
+}
+else
+{
+    console.log("Emp is Present");
+}
+
+//UC-2
+const IS_PART_TIME = 1 , IS_FULL_TIME = 2;
+const PART_TIME_HRS = 4 , FULL_TIME_HRS = 8;
+const WAGE_PER_HRS = 20;
+let empHrs = 0
+empCheck = Math.floor(Math.random() * 10 % 3 );
+switch (empCheck)
+{
+    case IS_PART_TIME:
+        empHrs=PART_TIME_HRS;
+        break;
+    case IS_FULL_TIME:
+        empHrs=FULL_TIME_HRS;
+        break;
+    default:
+        empHrs=0;
+}
+let empWage = empHrs * WAGE_PER_HRS;
+console.log("Emp. Wage :- " + empWage);
